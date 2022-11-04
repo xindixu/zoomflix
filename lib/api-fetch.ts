8ccb8b445cb0c7ffd48091c7450d5c6cdc7de995
Xindi Xu <xindixu0@gmail.com>
2022-11-04 17:34:42 -0400

    feat: get api endpoint

---
+++ lib/api-fetch.ts
@@ -0,0 +1,5 @@
+export const API_ENDPOINT =
+  "https://i9ltn1t7i4.execute-api.us-east-1.amazonaws.com/prod/"
+
+// TODO: use this function to fetch data from the api
+export const apiFetch = () => {}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next"
+import { API_ENDPOINT } from "../lib/api-fetch"
 
 const Home: NextPage = () => {
-  return <div>Zoomflex Home Page</div>
+  return (
+    <div>
+      Zoomflex Home Page
+      <div>Backend url: {API_ENDPOINT}</div>
+    </div>
+  )
 }
 
 export default Home
