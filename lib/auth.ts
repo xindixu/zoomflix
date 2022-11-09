d1274341bb68eab46b24c14363963a0086827ce8
Xindi Xu <xindixu0@gmail.com>
2022-11-09 13:28:19 -0500

    feat: add login/logout/signup form & pages

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+// import { Auth } from "aws-amplify"
+
+export function signUp({ email, password }) {
+  return () => {}
+}
+
+export function confirmSignUp({ email, code }) {
+  return () => {}
+}
+
+export function signIn({ email, password }) {
+  return () => {}
+}
+
+export async function signOut() {
+  return () => {}
+}
+
+export async function getCurrentUser() {
+  return () => {}
+}
